6172d34bdf08c80f452326c9d058696ea2882d50
patrick hrabos <phrabos@gmail.com>
2021-07-12 14:24:56 -0400

    add model and controller for post route

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const app = require('./lib/app');
 const pool = require('./pool');
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 const PORT = 8000;
+
 app.listen(PORT, () => {
 	console.log(`spinning on port ${PORT}`);
 });
